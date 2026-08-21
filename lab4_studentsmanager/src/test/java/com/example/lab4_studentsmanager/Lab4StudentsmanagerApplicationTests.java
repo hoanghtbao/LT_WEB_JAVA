8378c3fe1a330b4aaf0b3eac68d75d1868ed8633
hoanghtbao <hoanghtbao.dev@gmail.com>
2026-08-21 23:42:55 +0700

    Them bai tap lab4_studentsmanager

---
+++ lab4_studentsmanager/src/test/java/com/example/lab4_studentsmanager/Lab4StudentsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_studentsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4StudentsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
